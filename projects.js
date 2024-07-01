292a46ec1692f089202106ad1b52215ee5eb2c9d
Minseong0000 <dave711@naver.com>
2024-07-01 15:45:15 +0900

    update

--- projects.js
+++ projects.js
@@ -1,139 +1,110 @@
-gsap.registerPlugin(ScrollTrigger);
-
-ScrollTrigger.create({
-  trigger: "body",
-  start: "top top",
-  end: "bottom 300vh",
-  pin: ".portfolio-wrapper",
-  markers: false,
-  pinSpacing: false,
-});
-
-gsap.to(".portfolio-wrapper li", {
-  scrollTrigger: {
-    trigger: "body",
-    start: "top top",
-    end: "bottom bottom",
-    scrub: true,
-    markers: false,
+jQuery.event.special.wheel = {
+  setup: function (_, ns, handle) {
+    this.addEventListener("wheel", handle, { passive: false });
   },
-  y: "-312vh",
+};
+
+var slider = {
+  slideIds: ["slide-1", "slide-2", "slide-3", "slide-4"],
+  index: 0,
+  canScroll: true,
+  lastTimestamp: 0,
+  lastWheelDelta: 0,
+};
+
+function slideTo(slideIndex) {
+  slider.canScroll = false;
+  slider.index = slideIndex;
+  var translateY = -slideIndex * 100 + "vh"; // Calculate the translateY value
+  jQuery(".slide-container").css("transform", "translateY(" + translateY + ")"); // Apply transform
+  jQuery("a[href^='#']").removeClass("active-bullet");
+  jQuery('a[href="#' + slider.slideIds[slider.index] + '"]').addClass(
+    "active-bullet"
+  );
+
+  // Show or hide images based on slide index
+  if (slideIndex === 0) {
+    jQuery(".img-wrapper img").css("opacity", 1); // Show all images for the first slide
+  } else {
+    jQuery(".img-wrapper img").css("opacity", 1); // Show all images initially
+    jQuery(".img-wrapper img:lt(" + slideIndex + ")").css("opacity", 0); // Hide images up to the current slide index
+  }
+
+  // Scale images based on slide index
+  var totalSlides = slider.slideIds.length - 1; // Total number of slides (0-based index)
+  var scaleFactor = 1 + 0.2 * (slideIndex / totalSlides); // Calculate scale factor
+  jQuery(".img-wrapper img").css("transform", "scale(" + scaleFactor + ")");
+
+  // Change background color of .fixed-area based on slide index
+  var bgColor;
+  switch (slideIndex) {
+    case 0:
+      bgColor =
+        "linear-gradient(to right bottom, #fff, #ddd, rgba(77, 125, 96, 0.5))";
+      break;
+    case 1:
+      bgColor = "linear-gradient(to right bottom, #fff, #E0EBED)";
+      break;
+    case 2:
+      bgColor =
+        "linear-gradient(to right bottom, #fff, #ddd, rgba(88, 96, 98,.5)";
+      break;
+    case 3:
+      bgColor =
+        "linear-gradient(to right bottom, #fff, #ddd, rgba(164, 64, 64, 0.5))";
+      break;
+    default:
+      bgColor =
+        "linear-gradient(to right bottom, #fff, #ddd, rgba(77, 125, 96, 0.5))";
+  }
+  jQuery(".fixed-area").css("background", bgColor);
+
+  setTimeout(function () {
+    slider.canScroll = true;
+  }, 600);
+}
+
+jQuery(window).on("wheel", function (event) {
+  event.preventDefault();
+  var domEvent = event.originalEvent;
+  var delta = domEvent.deltaY;
+  var wheelDelta = domEvent.wheelDeltaY;
+  var timeDelta = Date.now() - slider.lastTimestamp;
+  slider.lastTimestamp = Date.now();
+  var wheelDeltaDelta = Math.abs(
+    Math.abs(wheelDelta) - Math.abs(slider.lastWheelDelta)
+  );
+
+  if (wheelDeltaDelta > 10) {
+    slider.lastTimestamp = 0;
+  }
+  slider.lastWheelDelta = wheelDelta;
+
+  if (slider.canScroll && timeDelta > 100) {
+    if (delta > 1) {
+      slider.index += 1;
+      if (slider.index >= slider.slideIds.length)
+        slider.index = slider.slideIds.length - 1;
+    } else if (delta < -1) {
+      slider.index -= 1;
+      if (slider.index < 0) slider.index = 0;
+    }
+
+    slideTo(slider.index);
+  }
 });
 
-gsap.to(".poster img", {
-  scale: 1.2,
-  scrollTrigger: {
-    trigger: "body",
-    start: "top top",
-    end: "bottom bottom",
-    scrub: true,
-    markers: false,
-  },
-  y: 50,
+jQuery.each(slider.slideIds, function (index, value) {
+  jQuery("#" + value).attr("data-index", index);
 });
 
-ScrollTrigger.create({
-  trigger: ".vivienne-westwood",
-  start: "200vh top",
-  onEnter: () => gsap.to(".img-container1 img", { display: "none" }),
-  onLeaveBack: () => gsap.to(".img-container1 img", { display: "block" }),
+jQuery("ul.bullets a[href^='#']").click(function (e) {
+  e.preventDefault();
+  var index = parseInt($($(this).attr("href")).attr("data-index"));
+  slideTo(index);
 });
 
-ScrollTrigger.create({
-  trigger: ".second",
-  start: "90vh top",
-  onEnter: () => gsap.to(".img-container2", { display: "none" }),
-  onLeaveBack: () => gsap.to(".img-container2", { display: "block" }),
-});
-
-ScrollTrigger.create({
-  trigger: ".third",
-  start: "90vh top",
-  onEnter: () => gsap.to(".img-container3", { display: "none" }),
-  onLeaveBack: () => gsap.to(".img-container3", { display: "block" }),
-});
-
-// ===================================
-
-ScrollTrigger.create({
-  trigger: ".vivienne-westwood",
-  start: "200vh top",
-  onEnter: () =>
-    gsap.to(".portfolio-wrapper", {
-      background: "linear-gradient(to right bottom, #fff, #E0EBED)",
-    }),
-  onLeaveBack: () =>
-    gsap.to(".portfolio-wrapper", {
-      background:
-        "linear-gradient(to right bottom, #fff, #ddd, rgb(77, 125, 96, 0.5))",
-    }),
-});
-
-ScrollTrigger.create({
-  trigger: ".second",
-  start: "90vh top",
-  onEnter: () => gsap.to(".portfolio-wrapper", { backgroundColor: "orange" }),
-  onLeaveBack: () =>
-    gsap.to(".portfolio-wrapper", { backgroundColor: "#f8b400" }),
-});
-
-ScrollTrigger.create({
-  trigger: ".third",
-  start: "90vh top",
-  onEnter: () => gsap.to(".portfolio-wrapper", { backgroundColor: "red" }),
-  onLeaveBack: () =>
-    gsap.to(".portfolio-wrapper", { backgroundColor: "orange" }),
-});
-
-// =======================================
-
-ScrollTrigger.create({
-  trigger: ".vivienne-westwood",
-  start: "90vh top",
-  onEnter: () =>
-    gsap.to(".portfolio-list ul li:first-child", { color: "#9F9F9F" }),
-  onLeaveBack: () =>
-    gsap.to(".portfolio-list ul li:first-child", { color: "#000" }),
-});
-ScrollTrigger.create({
-  trigger: ".vivienne-westwood",
-  start: "90vh top",
-  onEnter: () =>
-    gsap.to(".portfolio-list ul li:nth-child(2)", { color: "#000" }),
-  onLeaveBack: () =>
-    gsap.to(".portfolio-list ul li:nth-child(2)", { color: "#9f9f9f" }),
-});
-
-ScrollTrigger.create({
-  trigger: ".second",
-  start: "90vh top",
-  onEnter: () =>
-    gsap.to(".portfolio-list ul li:nth-child(2)", { color: "#9f9f9f" }),
-  onLeaveBack: () =>
-    gsap.to(".portfolio-list ul li:nth-child(2)", { color: "#000" }),
-});
-ScrollTrigger.create({
-  trigger: ".second",
-  start: "90vh top",
-  onEnter: () =>
-    gsap.to(".portfolio-list ul li:nth-child(3)", { color: "#000" }),
-  onLeaveBack: () =>
-    gsap.to(".portfolio-list ul li:nth-child(3)", { color: "#9f9f9f" }),
-});
-
-ScrollTrigger.create({
-  trigger: ".third",
-  start: "90vh top",
-  onEnter: () =>
-    gsap.to(".portfolio-list ul li:nth-child(3)", { color: "#9f9f9f" }),
-  onLeaveBack: () =>
-    gsap.to(".portfolio-list ul li:nth-child(3)", { color: "#000" }),
-});
-ScrollTrigger.create({
-  trigger: ".third",
-  start: "90vh top",
-  onEnter: () =>
-    gsap.to(".portfolio-list ul li:nth-child(4)", { color: "#000" }),
-  onLeaveBack: () =>
-    gsap.to(".portfolio-list ul li:nth-child(4)", { color: "#9f9f9f" }),
+// Enable click on links inside slides
+jQuery(".description a").click(function (e) {
+  e.stopPropagation();
 });
